c6bac660ce2f07bb6df86ce98acb73e1e164209a
apignat1 <142963620+apignat1@users.noreply.github.com>
2023-08-23 02:16:42 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -22,3 +22,23 @@ for i in range(len(tempf[:,0])):
 merged.iloc[:,1]=pd.Series(tempf[:,0])
 # Save dataframe in drive (then download and upload to github)
 merged.to_csv('/content/drive/My Drive/merged_dataframe.csv', index=False)
+
+# Color-code for plotting
+# select different causes
+causes = merged.iloc[:,4].unique()
+# check length and create array of colors
+len(causes)
+col_options = ['black', 'red', 'green', 'blue', 'cyan','orange','grey','purple','pink','brown']
+colors=np.empty(len(merged.iloc[:,4]),dtype='object')
+for i in range(len(colors)):
+  for j in range(len(col_options)):
+    if merged.iloc[i,4] == causes[j]:
+      colors[i] = col_options[j]
+
+# Plot with legend
+import matplotlib.pyplot as plt
+plt.bar(merged.iloc[:,0],merged.iloc[:,1],color=colors)
+plt.xlabel('Year')
+plt.ylabel('Death Toll')
+handles = [plt.Rectangle((0,0),1,1, color=col_options[i]) for i in range(len(causes))]
+plt.legend(handles, causes)
