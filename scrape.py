14407fe0ffd9895655a0592488514c5103b0b929
apignat1 <142963620+apignat1@users.noreply.github.com>
2023-08-23 00:40:15 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,1 +1,6 @@
-
+from urllib.request import urlopen
+url = "https://en.wikipedia.org/wiki/List_of_natural_disasters_by_death_toll"
+page = urlopen(url)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+print(html)
